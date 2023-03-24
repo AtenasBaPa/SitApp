67f6e0ef3178656990b2fae7cd2e9f2073207444
JUAN BERNAL MARTINEZ <juan.bernalm@alumno.buap.mx>
2023-03-23 21:31:46 -0600

    Navbar hecho

--- panel/panel.php
+++ panel/panel.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sources/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     
+<nav class="navbar">
+  <div class="navbar-container">
+    <a href="#" class="logo"><img src="sources/img/SIT-APP.svg"></a>
+    <div class="navbar-links">
+      <a href="#">NOSOTROS</a>
+      <a href="#" class="login-btn">INICIAR SESIÓN</a>
+    </div>
+    <div class="navbar-responsive">
+      <span class="icon">&#9776;</span>
+    </div>
+  </div>
+</nav>
+
+
 </body>
 </html>
