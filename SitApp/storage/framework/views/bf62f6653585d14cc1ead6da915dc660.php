90ef508294beb667c5dd56901ac986075c1d3674
EduCastCh <eduardocastch@gmail.com>
2023-03-27 23:57:02 -0600

    implementacion
    index, plantillas y controladores

---
+++ SitApp/storage/framework/views/bf62f6653585d14cc1ead6da915dc660.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\SitApp\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ SitApp/storage/framework/views/e0b8a8e289a183f3516fa0161478ef84.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('content'); ?>
+
+<div><p>ñ{saljdbnñoawubdñawoidbnañdjilbwad</p></div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\SitApp\resources\views/nueva.blade.php ENDPATH**/ ?>
